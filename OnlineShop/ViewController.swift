9b2680e838ea4d1775b1d3e1ded65df01267c8d1
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-08-10 20:45:31 +0600

    Initial Commit

---
+++ OnlineShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnlineShop
+//
+//  Created by 4eenah on 8/10/20.
+//  Copyright © 2020 Tleugazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
